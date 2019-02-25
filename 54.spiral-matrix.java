6d2c05d75a08213e316d40ee55ba2950f7c2cb9e
周长安 <zchangan@163.com>
2019-02-25 15:47:29 +0800

    54.spiral-matrix

---
+++ 54.spiral-matrix.java
@@ -0,0 +1,71 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * @lc app=leetcode id=54 lang=java
+ *
+ * [54] Spiral Matrix
+ *
+ * https://leetcode.com/problems/spiral-matrix/description/
+ *
+ * algorithms
+ * Medium (29.51%)
+ * Total Accepted:    208.4K
+ * Total Submissions: 705.3K
+ * Testcase Example:  '[[1,2,3],[4,5,6],[7,8,9]]'
+ *
+ * Given a matrix of m x n elements (m rows, n columns), return all elements of
+ * the matrix in spiral order.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input:
+ * [
+ * ⁠[ 1, 2, 3 ],
+ * ⁠[ 4, 5, 6 ],
+ * ⁠[ 7, 8, 9 ]
+ * ]
+ * Output: [1,2,3,6,9,8,7,4,5]
+ * 
+ * 
+ * Example 2:
+ * 
+ * Input:
+ * [
+ * ⁠ [1, 2, 3, 4],
+ * ⁠ [5, 6, 7, 8],
+ * ⁠ [9,10,11,12]
+ * ]
+ * Output: [1,2,3,4,8,12,11,10,9,5,6,7]
+ * 
+ */
+/**
+ * Use Approach 2 in Solution: Layer-by-Layer
+ * 模拟
+ * 使用 r1 记录当前能走的最小行， r2 为最大行
+ * 使用 c1 记录当前能走的最小列， c2 为最大列
+ * 然后向四个方向走，走完一圈更新上面的值即可
+ */
+class Solution {
+    public List < Integer > spiralOrder(int[][] matrix) {
+        List<Integer> ans = new ArrayList<>();
+        if (matrix.length == 0)
+            return ans;
+        int r1 = 0, r2 = matrix.length - 1;
+        int c1 = 0, c2 = matrix[0].length - 1;
+        while (r1 <= r2 && c1 <= c2) {
+            for (int c = c1; c <= c2; c++) ans.add(matrix[r1][c]);
+            for (int r = r1 + 1; r <= r2; r++) ans.add(matrix[r][c2]);
+            if (r1 < r2 && c1 < c2) {
+                for (int c = c2 - 1; c > c1; c--) ans.add(matrix[r2][c]);
+                for (int r = r2; r > r1; r--) ans.add(matrix[r][c1]);
+            }
+            r1++;
+            r2--;
+            c1++;
+            c2--;
+        }
+        return ans;
+    }
+}
