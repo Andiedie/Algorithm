c18fee16d2c98a65c1007d482856011b542d50a9
周长安 <zchangan@163.com>
2019-03-03 00:47:23 +0800

    200.number-of-islands

---
+++ 200.number-of-islands.java
@@ -0,0 +1,67 @@
+/*
+ * @lc app=leetcode id=200 lang=java
+ *
+ * [200] Number of Islands
+ *
+ * https://leetcode.com/problems/number-of-islands/description/
+ *
+ * algorithms
+ * Medium (40.13%)
+ * Total Accepted:    303.9K
+ * Total Submissions: 754.8K
+ * Testcase Example:  '[["1","1","1","1","0"],["1","1","0","1","0"],["1","1","0","0","0"],["0","0","0","0","0"]]'
+ *
+ * Given a 2d grid map of '1's (land) and '0's (water), count the number of
+ * islands. An island is surrounded by water and is formed by connecting
+ * adjacent lands horizontally or vertically. You may assume all four edges of
+ * the grid are all surrounded by water.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input:
+ * 11110
+ * 11010
+ * 11000
+ * 00000
+ * 
+ * Output: 1
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input:
+ * 11000
+ * 11000
+ * 00100
+ * 00011
+ * 
+ * Output: 3
+ * 
+ */
+/**
+ * Use solution https://leetcode.com/problems/number-of-islands/discuss/56349/7-lines-Python-~14-lines-Java
+ * 沉没！
+ * 每找到一块陆地，就将周围的所有陆地递归沉下去，并将计数器加一
+ */
+class Solution {
+    public int numIslands(char[][] grid) {
+        int count = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                count += sink(grid, i, j);
+            }
+        }
+        return count;
+    }
+    private int sink(char[][] grid, int i, int j) {
+        if (i < 0 || i == grid.length || j < 0 || j == grid[i].length || grid[i][j] == '0') {
+            return 0;
+        }
+        grid[i][j] = '0';
+        sink(grid, i + 1, j); sink(grid, i - 1, j); sink(grid, i, j + 1); sink(grid, i, j - 1);
+        return 1;
+    }
+}
+
