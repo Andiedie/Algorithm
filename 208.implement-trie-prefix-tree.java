03555a2c46a963932298eaabda1178f8c64454b2
周长安 <zchangan@163.com>
2019-03-03 11:26:55 +0800

    208.implement-trie-prefix-tree

---
+++ 208.implement-trie-prefix-tree.java
@@ -0,0 +1,128 @@
+/*
+ * @lc app=leetcode id=208 lang=java
+ *
+ * [208] Implement Trie (Prefix Tree)
+ *
+ * https://leetcode.com/problems/implement-trie-prefix-tree/description/
+ *
+ * algorithms
+ * Medium (36.55%)
+ * Total Accepted:    162.2K
+ * Total Submissions: 441.5K
+ * Testcase Example:  '["Trie","insert","search","search","startsWith","insert","search"]\n[[],["apple"],["apple"],["app"],["app"],["app"],["app"]]'
+ *
+ * Implement a trie with insert, search, and startsWith methods.
+ * 
+ * Example:
+ * 
+ * 
+ * Trie trie = new Trie();
+ * 
+ * trie.insert("apple");
+ * trie.search("apple");   // returns true
+ * trie.search("app");     // returns false
+ * trie.startsWith("app"); // returns true
+ * trie.insert("app");   
+ * trie.search("app");     // returns true
+ * 
+ * 
+ * Note:
+ * 
+ * 
+ * You may assume that all inputs are consist of lowercase letters a-z.
+ * All inputs are guaranteed to be non-empty strings.
+ * 
+ * 
+ */
+/**
+ * Use solution in https://leetcode.com/problems/implement-trie-prefix-tree/solution/
+ * 字典树 或 前缀树
+ * 自动完成、拼写检查、路由表最长前缀匹配、九键盘预测、填字游戏等都可以使用
+ * 相比 哈希树 和 搜索树 可以在 O(1) / O(h) 时间内搜索到
+ * 字典树需要 O(m) 时间， m 是被搜索单词长度
+ * 但是字典树可以搜索所有前缀相同的词，可以字典序遍历所有词语等
+ */
+class Trie {
+    class TrieNode {
+
+        // R links to node children
+        private TrieNode[] links;
+    
+        private final int R = 26;
+    
+        private boolean isEnd;
+    
+        public TrieNode() {
+            links = new TrieNode[R];
+        }
+    
+        public boolean containsKey(char ch) {
+            return links[ch -'a'] != null;
+        }
+        public TrieNode get(char ch) {
+            return links[ch -'a'];
+        }
+        public void put(char ch, TrieNode node) {
+            links[ch -'a'] = node;
+        }
+        public void setEnd() {
+            isEnd = true;
+        }
+        public boolean isEnd() {
+            return isEnd;
+        }
+    }
+
+    private TrieNode searchPrefix(String word) {
+        TrieNode node = root;
+        for (int i = 0; i < word.length(); i++) {
+           char curLetter = word.charAt(i);
+           if (node.containsKey(curLetter)) {
+               node = node.get(curLetter);
+           } else {
+               return null;
+           }
+        }
+        return node;
+    }
+
+    /** Initialize your data structure here. */
+    private TrieNode root;
+    public Trie() {
+        root = new TrieNode();
+    }
+    
+    /** Inserts a word into the trie. */
+    public void insert(String word) {
+        TrieNode node = root;
+        for (int i = 0; i < word.length(); i++) {
+            char currentChar = word.charAt(i);
+            if (!node.containsKey(currentChar)) {
+                node.put(currentChar, new TrieNode());
+            }
+            node = node.get(currentChar);
+        }
+        node.setEnd();
+    }
+    
+    /** Returns if the word is in the trie. */
+    public boolean search(String word) {
+        TrieNode node = searchPrefix(word);
+        return node != null && node.isEnd();
+    }
+    
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    public boolean startsWith(String prefix) {
+        TrieNode node = searchPrefix(prefix);
+        return node != null;
+    }
+}
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
+
