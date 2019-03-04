7bc9aec49f2ed5dc6ee5148839a705d06f3e0c6f
周长安 <zchangan@163.com>
2019-03-04 20:12:21 +0800

    230.kth-smallest-element-in-a-bst

---
+++ 230.kth-smallest-element-in-a-bst.java
@@ -0,0 +1,92 @@
+/*
+ * @lc app=leetcode id=230 lang=java
+ *
+ * [230] Kth Smallest Element in a BST
+ *
+ * https://leetcode.com/problems/kth-smallest-element-in-a-bst/description/
+ *
+ * algorithms
+ * Medium (49.74%)
+ * Total Accepted:    201.8K
+ * Total Submissions: 403.9K
+ * Testcase Example:  '[3,1,4,null,2]\n1'
+ *
+ * Given a binary search tree, write a function kthSmallest to find the kth
+ * smallest element in it.
+ * 
+ * Note: 
+ * You may assume k is always valid, 1 ≤ k ≤ BST's total elements.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: root = [3,1,4,null,2], k = 1
+ * ⁠  3
+ * ⁠ / \
+ * ⁠1   4
+ * ⁠ \
+ * 2
+ * Output: 1
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: root = [5,3,6,2,4,null,null,1], k = 3
+ * ⁠      5
+ * ⁠     / \
+ * ⁠    3   6
+ * ⁠   / \
+ * ⁠  2   4
+ * ⁠ /
+ * ⁠1
+ * Output: 3
+ * 
+ * 
+ * Follow up:
+ * What if the BST is modified (insert/delete operations) often and you need to
+ * find the kth smallest frequently? How would you optimize the kthSmallest
+ * routine?
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+/**
+ * Use solution in https://leetcode.com/problems/kth-smallest-element-in-a-bst/discuss/63660/3-ways-implemented-in-JAVA-(Python)%3A-Binary-Search-in-order-iterative-and-recursive
+ * 深度优先搜索 中序遍历
+ * 初始化计数器为 k
+ * 递归的“递”过程，从 root 节点一路向左到最左子节点
+ * 归：每次给计数器减 1，计数器为 0 时表示找到 kth
+ * 
+ */
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
+class Solution {
+    private int k = 0;
+    private int ans = 0;
+    public int kthSmallest(TreeNode root, int k) {
+        this.k = k;
+        dfs(root);
+        return ans;
+    }
+    private void dfs(TreeNode node) {
+        if (node.left != null) dfs(node.left);
+        k--;
+        if (k == 0) {
+            ans = node.val;
+            return;
+        }
+        if (node.right != null) dfs(node.right); 
+    }
+}
+
