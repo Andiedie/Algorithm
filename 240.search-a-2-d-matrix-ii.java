175295fc0cc4d9ac52209137f5cd21d09e2d5f76
周长安 <zchangan@163.com>
2019-03-05 10:47:22 +0800

    240.search-a-2-d-matrix-ii

---
+++ 240.search-a-2-d-matrix-ii.java
@@ -0,0 +1,65 @@
+/*
+ * @lc app=leetcode id=240 lang=java
+ *
+ * [240] Search a 2D Matrix II
+ *
+ * https://leetcode.com/problems/search-a-2d-matrix-ii/description/
+ *
+ * algorithms
+ * Medium (40.17%)
+ * Total Accepted:    161.8K
+ * Total Submissions: 401.9K
+ * Testcase Example:  '[[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]]\n5'
+ *
+ * Write an efficient algorithm that searches for a value in an m x n matrix.
+ * This matrix has the following properties:
+ * 
+ * 
+ * Integers in each row are sorted in ascending from left to right.
+ * Integers in each column are sorted in ascending from top to bottom.
+ * 
+ * 
+ * Example:
+ * 
+ * Consider the following matrix:
+ * 
+ * 
+ * [
+ * ⁠ [1,   4,  7, 11, 15],
+ * ⁠ [2,   5,  8, 12, 19],
+ * ⁠ [3,   6,  9, 16, 22],
+ * ⁠ [10, 13, 14, 17, 24],
+ * ⁠ [18, 21, 23, 26, 30]
+ * ]
+ * 
+ * 
+ * Given target = 5, return true.
+ * 
+ * Given target = 20, return false.
+ * 
+ */
+/**
+ * Use solution in https://leetcode.com/problems/search-a-2d-matrix-ii/discuss/66140/My-concise-O(m%2Bn)-Java-solution
+ * 我们从矩阵的右上角开始搜索
+ * 这个位置上，该行的所有元素都比它小，该列的所有元素都比它大
+ * 如果目标元素比该元素小，则目标元素一定不在这一列上，可以排除这一列
+ * 如果目标元素比该元素大，则目标元素一定不在这一行上，可以排除这一行
+ * 直到所有行列都被探索或者该元素正好等于目标，返回结果
+ */
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix.length < 1 || matrix[0].length < 1) return false;
+        int row = 0, col = matrix[0].length - 1;
+        while (col >= 0 && row <= matrix.length - 1) {
+            if (target < matrix[row][col]) {
+                col--;
+            } else if (target > matrix[row][col]) {
+                row++;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
