601e5553922bdf0f4dc5673a6f7e035acc08714b
周长安 <zchangan@163.com>
2019-03-08 12:07:33 +0800

    289.game-of-life

---
+++ 289.game-of-life.java
@@ -0,0 +1,123 @@
+/*
+ * @lc app=leetcode id=289 lang=java
+ *
+ * [289] Game of Life
+ *
+ * https://leetcode.com/problems/game-of-life/description/
+ *
+ * algorithms
+ * Medium (43.12%)
+ * Total Accepted:    101.6K
+ * Total Submissions: 233.5K
+ * Testcase Example:  '[[0,1,0],[0,0,1],[1,1,1],[0,0,0]]'
+ *
+ * According to the Wikipedia's article: "The Game of Life, also known simply
+ * as Life, is a cellular automaton devised by the British mathematician John
+ * Horton Conway in 1970."
+ * 
+ * Given a board with m by n cells, each cell has an initial state live (1) or
+ * dead (0). Each cell interacts with its eight neighbors (horizontal,
+ * vertical, diagonal) using the following four rules (taken from the above
+ * Wikipedia article):
+ * 
+ * 
+ * Any live cell with fewer than two live neighbors dies, as if caused by
+ * under-population.
+ * Any live cell with two or three live neighbors lives on to the next
+ * generation.
+ * Any live cell with more than three live neighbors dies, as if by
+ * over-population..
+ * Any dead cell with exactly three live neighbors becomes a live cell, as if
+ * by reproduction.
+ * 
+ * 
+ * Write a function to compute the next state (after one update) of the board
+ * given its current state. The next state is created by applying the above
+ * rules simultaneously to every cell in the current state, where births and
+ * deaths occur simultaneously.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: 
+ * [
+ * [0,1,0],
+ * [0,0,1],
+ * [1,1,1],
+ * [0,0,0]
+ * ]
+ * Output: 
+ * [
+ * [0,0,0],
+ * [1,0,1],
+ * [0,1,1],
+ * [0,1,0]
+ * ]
+ * 
+ * 
+ * Follow up:
+ * 
+ * 
+ * Could you solve it in-place? Remember that the board needs to be updated at
+ * the same time: You cannot update some cells first and then use their updated
+ * values to update other cells.
+ * In this question, we represent the board using a 2D array. In principle, the
+ * board is infinite, which would cause problems when the active area
+ * encroaches the border of the array. How would you address these problems?
+ * 
+ * 
+ */
+/**
+ * Use solution in https://leetcode.com/problems/game-of-life/discuss/73223/Easiest-JAVA-solution-with-explanation
+ * 经典的生命游戏
+ * 模拟
+ * 需要注意的是，数组必须同步更新
+ * 可以使用二进制位来表示状态
+ * 首先假设某个位上：
+ * 1 表示 生
+ * 0 表示 死
+ * 然后用 int 的最低两个位来表示每个细胞的状态：
+ * [下一回合的状态][本回合的状态]
+ * 00：下回合死，本回合死
+ * 01：下回合死，本回合生
+ * 10：下回合生，本回合死
+ * 11：下回合生，本回合生
+ * 
+ * 默认的初始状态，0 1 正好对应了 00 和 01
+ * 
+ * 如何获取当前细胞的状态，int & 1 （取最低位即可）
+ * 如何设置细胞状态，根据细胞本回合和下回合的状态，设置为 1、2、3、4
+ * 如何同步更新状态，所有细胞都 int >> 1 （将倒数第二位右移到最后一位）
+ */
+class Solution {
+    public void gameOfLife(int[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                int lives = liveNeighbors(board, i, j);
+                if (board[i][j] == 1 && lives >= 2 && lives <= 3) {
+                    board[i][j] = 3;
+                } else if (board[i][j] == 0 && lives == 3) {
+                    board[i][j] = 2;
+                }
+            }
+        }
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                board[i][j] >>= 1;
+            }
+        }
+    }
+    private int liveNeighbors(int[][] board, int x, int y) {
+        int count = 0;
+        for (int i = -1; i <= 1; i++) {
+            for (int j = -1; j <= 1; j++) {
+                if (i == 0 && j == 0) continue;
+                if (x + i >= 0 && x + i < board.length && y + j >= 0 && y + j < board[x + i].length) {
+                    count += board[x + i][y + j] & 1;
+                }
+            }
+        }
+        return count;
+    }
+}
+
