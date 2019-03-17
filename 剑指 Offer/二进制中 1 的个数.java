e47d41f0d381c105b6b2f8f65cbbf6c491272315
周长安 <zchangan@163.com>
2019-03-17 21:55:55 +0800

    二进制中 1 的个数

---
+++ 剑指 Offer/二进制中 1 的个数.java
@@ -0,0 +1,27 @@
+/**
+ * 三种方法
+ * 见注释
+ */
+class Solution {
+  public int NumberOf1(int n) {
+      // 直接使用 Java 的函数
+      // return Integer.bitCount(n);
+
+      // 使用无符号右移
+      // 每次通过 与1 取最低位
+      // int count = 0;
+      // while (n != 0) {
+      //     count += n & 1;
+      //     n >>>= 1;
+      // }
+      // return count;
+
+      // 使用 n & (n - 1) 删除最低位的一个 1
+      int count = 0;
+      while (n != 0) {
+          count++;
+          n = n & (n - 1);
+      }
+      return count;
+  }
+}
