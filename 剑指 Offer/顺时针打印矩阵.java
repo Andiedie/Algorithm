4b7ef6b67b4b08d3cb66dbd79f9788fa8988adfd
周长安 <zchangan@163.com>
2019-03-18 00:37:11 +0800

    顺时针打印矩阵

---
+++ 剑指 Offer/顺时针打印矩阵.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+/**
+ * 输入一个数组，顺时针回字形打印所有数字
+ * 同 LeetCode [54] Spiral Matrix
+ * 使用 r1 记录当前能走的最小行， r2 为最大行
+ * 使用 c1 记录当前能走的最小列， c2 为最大列
+ * 然后向四个方向走，走完一圈更新上面的值即可
+ */
+
+class Solution {
+    public ArrayList<Integer> printMatrix(int [][] matrix) {
+        ArrayList<Integer> ans = new ArrayList<>();
+        if (matrix.length == 0) return ans;
+        int r1 = 0, r2 = matrix.length - 1;
+        int c1 = 0, c2 = matrix[0].length - 1;
+        while (r1 <= r2 && c1 <= c2) {
+            for (int c = c1; c <= c2; c++) ans.add(matrix[r1][c]);
+            for (int r = r1 + 1; r <= r2; r++) ans.add(matrix[r][c2]);
+            if (r1 < r2 && c1 < c2) {
+                for (int c = c2 - 1; c > c1; c--) ans.add(matrix[r2][c]);
+                for (int r = r2; r > r1; r--) ans.add(matrix[r][c1]);
+            }
+            r1++;
+            r2--;
+            c1++;
+            c2--;
+        }
+        return ans;
+    }
+}
