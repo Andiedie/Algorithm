7da089255f472a6be16f6caf8de5696ba8df500c
周长安 <zchangan@163.com>
2019-03-17 23:39:41 +0800

    反转链表

---
+++ 剑指 Offer/反转链表.java
@@ -0,0 +1,34 @@
+/**
+ * 输入一个链表，反转链表后，输出新链表的表头。
+ * 
+ * 用三个指针
+ */
+
+class Solution {
+  public ListNode ReverseList(ListNode head) {
+      ListNode left = head;
+      if (left == null) return null;
+      ListNode mid = head.next;
+      if (mid == null) return left;
+      ListNode right = mid.next;
+      left.next = null;
+      mid.next = left;
+      while (right != null) {
+          ListNode next = right.next;
+          right.next = mid;
+          left = mid;
+          mid = right;
+          right = next;
+      }
+      return mid;
+  }
+}
+
+class ListNode {
+  int val;
+  ListNode next = null;
+
+  ListNode(int val) {
+      this.val = val;
+  }
+}
