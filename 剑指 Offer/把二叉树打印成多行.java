c7f48d7833884089bfd87fa2a1e3e5ad0b554d82
周长安 <zchangan@163.com>
2019-03-21 16:30:26 +0800

    把二叉树打印成多行

---
+++ 剑指 Offer/把二叉树打印成多行.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 从上到下按层打印二叉树，同一层结点从左至右输出。每一层输出一行。
+ * 二叉树层次遍历
+ * 直接使用 队列 实现 BFS
+ */
+
+class Solution {
+    ArrayList<ArrayList<Integer>> Print(TreeNode pRoot) {
+        ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
+        if (pRoot == null) return ans;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(pRoot);
+        while (!queue.isEmpty()) {
+            int num = queue.size();
+            ArrayList<Integer> list = new ArrayList<>(num);
+            while (num-- > 0) {
+                TreeNode node = queue.poll();
+                list.add(node.val);
+                if (node.left != null) queue.add(node.left);
+                if (node.right != null) queue.add(node.right);
+            }
+            ans.add(list);
+        }
+        return ans;
+    }
+}
+
+class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+
+    }
+
+}
